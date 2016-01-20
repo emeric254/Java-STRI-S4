3c78a317b098836114511e6e0c0fbf0629b30b06
emeric254 <emeric254@live.fr>
2016-01-20 10:45:57 +0100

    merge modele

--- Code/java-connect/src/main/java/stri/java_connect/modele/Utilisateur.java
+++ Code/java-connect/src/main/java/stri/java_connect/modele/Utilisateur.java
@@ -1,5 +1,7 @@
 package stri.java_connect.modele;
 
+import java.util.ArrayDeque;
+
 /**
  * Class Utilisateur
  */
@@ -11,6 +13,16 @@ public class Utilisateur {
 
   private String identifiant;
   private String motDePasse;
+  
+  private String nom;
+  private Long dateDiplome;
+  private String telephone;
+  private String courriel;
+  private String permissionLecture;
+
+  private String privilege;
+
+  private ArrayDeque<String> Competences;
   
   //
   // Constructors
@@ -32,7 +44,7 @@ public class Utilisateur {
    * Set the value of identifiant
    * @param newVar the new value of identifiant
    */
-  private void setIdentifiant (String newVar)
+  public void setIdentifiant (String newVar)
   {
     identifiant = newVar;
   }
@@ -41,7 +53,7 @@ public class Utilisateur {
    * Get the value of identifiant
    * @return the value of identifiant
    */
-  private String getIdentifiant ()
+  public String getIdentifiant ()
   {
     return identifiant;
   }
@@ -50,7 +62,7 @@ public class Utilisateur {
    * Set the value of motDePasse
    * @param newVar the new value of motDePasse
    */
-  private void setMotDePasse (String newVar)
+  public void setMotDePasse (String newVar)
   {
     motDePasse = newVar;
   }
@@ -59,9 +71,135 @@ public class Utilisateur {
    * Get the value of motDePasse
    * @return the value of motDePasse
    */
-  private String getMotDePasse ()
+  public String getMotDePasse ()
   {
     return motDePasse;
+  }
+
+  /**
+   * Set the value of nom
+   * @param newVar the new value of nom
+   */
+  public void setNom (String newVar)
+  {
+    nom = newVar;
+  }
+
+  /**
+   * Get the value of nom
+   * @return the value of nom
+   */
+  public String getNom ()
+  {
+    return nom;
+  }
+
+  /**
+   * Set the value of dateDiplome
+   * @param newVar the new value of dateDiplome
+   */
+  public void setDateDiplome (Long newVar)
+  {
+    dateDiplome = newVar;
+  }
+
+  /**
+   * Get the value of dateDiplome
+   * @return the value of dateDiplome
+   */
+  public Long getDateDiplome ()
+  {
+    return dateDiplome;
+  }
+
+  /**
+   * Set the value of telephone
+   * @param newVar the new value of telephone
+   */
+  public void setTelephone (String newVar)
+  {
+    telephone = newVar;
+  }
+
+  /**
+   * Get the value of telephone
+   * @return the value of telephone
+   */
+  public String getTelephone ()
+  {
+    return telephone;
+  }
+
+  /**
+   * Set the value of courriel
+   * @param newVar the new value of courriel
+   */
+  public void setCourriel (String newVar)
+  {
+    courriel = newVar;
+  }
+
+  /**
+   * Get the value of courriel
+   * @return the value of courriel
+   */
+  public String getCourriel ()
+  {
+    return courriel;
+  }
+
+  /**
+   * Set the value of permissionLecture
+   * @param newVar the new value of permissionLecture
+   */
+  public void setPermissionLecture (String newVar)
+  {
+    permissionLecture = newVar;
+  }
+
+  /**
+   * Get the value of permissionLecture
+   * @return the value of permissionLecture
+   */
+  public String getPermissionLecture ()
+  {
+    return permissionLecture;
+  }
+
+  /**
+   * Set the value of privilege
+   * @param newVar the new value of privilege
+   */
+  public void setPrivilege (String newVar)
+  {
+	  privilege = newVar;
+  }
+
+  /**
+   * Get the value of privilege
+   * @return the value of privilege
+   */
+  public String getPrivilege ()
+  {
+    return privilege;
+  }
+
+  /**
+   * Get the value of Competences
+   * @return the value of Competences
+   */
+  public ArrayDeque<String> getCompetences ()
+  {
+    return Competences;
+  }
+
+  /**
+   * Set the value of Competences
+   * @param newVar the new value of Competences
+   */
+  public void setCompetences (ArrayDeque<String> newVar)
+  {
+	  Competences = newVar;
   }
 
   //
