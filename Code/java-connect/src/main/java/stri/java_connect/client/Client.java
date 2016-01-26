dd924c3acc5994f9d6d9c537ec47d8727e073ec8
emeric254 <emeric254@live.fr>
2016-01-26 22:14:22 +0100

    ajout client tcp generique 	nouveau fichier : Code/java-connect/src/main/java/stri/java_connect/client/Client.java

---
+++ Code/java-connect/src/main/java/stri/java_connect/client/Client.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package stri.java_connect.client;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * @author emeric
+ *
+ */
+public class Client
+{
+    private int port;
+    String reponse;
+    Socket socketService;
+    PrintStream fluxSortieSocket;
+    BufferedReader fluxEntreeSocket;
+
+    public Client(int pPort)
+    {
+    	port = pPort;
+        try
+        {
+            socketService= new Socket("127.0.0.1", port);
+            fluxEntreeSocket = new BufferedReader(new InputStreamReader(socketService.getInputStream()));
+            fluxSortieSocket = new PrintStream(socketService.getOutputStream());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println("Connexion établie sur 127.0.0.1 " + port);
+    }
+
+    public String communiquer(String message) throws IOException
+    {
+        fluxSortieSocket.println(message);
+        fluxSortieSocket.flush();
+        return fluxEntreeSocket.readLine();
+    }
+    
+    public void fermer()
+    {
+        try
+        {
+            this.socketService.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
