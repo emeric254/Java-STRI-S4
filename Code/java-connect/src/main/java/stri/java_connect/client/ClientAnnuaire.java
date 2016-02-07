41a756d0dd4dda52b15830ca8deddfa7c248874f
emeric254 <emeric254@live.fr>
2016-02-07 18:43:35 +0100

    ajout methodes clientAnnuaire

--- Code/java-connect/src/main/java/stri/java_connect/client/ClientAnnuaire.java
+++ Code/java-connect/src/main/java/stri/java_connect/client/ClientAnnuaire.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import stri.java_connect.modele.Utilisateur;
 import stri.java_connect.protocol.ProtocoleAnnuaire;
+import stri.java_connect.utils.MD5Hasher;
 
 /**
  * @author emeric
@@ -54,6 +55,17 @@ public class ClientAnnuaire
 	}
 	
 	/**
+	 * @param courriel
+	 * @param mdp
+	 * @throws IOException 
+	 */
+	public void connexionMD5(String courriel, String mdp) throws IOException
+	{
+		// TODO
+		System.out.println(client.communiquer(ProtocoleAnnuaire.requeteConnexionHashMD5(courriel,MD5Hasher.hashString(mdp))));
+	}
+	
+	/**
 	 * @throws IOException 
 	 * 
 	 */
@@ -71,5 +83,33 @@ public class ClientAnnuaire
 	{
 		// TODO
 		System.out.println(client.communiquer(ProtocoleAnnuaire.requeteConsulterProfil(courriel)));
+	}
+	
+	/**
+	 * @param utilisateur
+	 * @throws IOException 
+	 */
+	public void modificationProfil(Utilisateur utilisateur) throws IOException
+	{
+		// TODO
+		System.out.println(client.communiquer(ProtocoleAnnuaire.requeteModifierProfil(utilisateur.getCourriel(), utilisateur.toString())));
+	}
+	
+	/**
+	 * @param courriel
+	 * @throws IOException 
+	 */
+	public void suppressionProfil(String courriel) throws IOException
+	{
+		// TODO
+		System.out.println(client.communiquer(ProtocoleAnnuaire.requeteSuppressionProfil(courriel)));
+	}
+	
+	/**
+	 * 
+	 */
+	public void deconnexion()
+	{
+		client.fermer();
 	}
 }
