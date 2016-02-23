d75673a60b3817204cb17b8c773707254d5db8a4
emeric254 <emeric254@live.fr>
2016-02-23 21:14:08 +0100

    classes crees

---
+++ Code/java-connect/src/main/java/stri/java_connect/protocol/ControlleurProtocoleMessagerie.java
@@ -0,0 +1,30 @@
+package stri.java_connect.protocol;
+
+//import stri.java_connect.modele.Utilisateur;
+
+public class ControlleurProtocoleMessagerie extends ControlleurProtocole
+{
+	//private Utilisateur utilisateur;
+
+	@Override
+	public String traiterRequete(String requete)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String traiterReponse(String reponse)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ControlleurProtocoleMessagerie clone()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
