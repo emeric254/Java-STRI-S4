baaf9593bbd2f04f2a38635f63fcfdf21757ea8d
emeric254 <emeric254@live.fr>
2016-02-26 17:55:28 +0100

    protocole messagerie un peu complete

--- Code/java-connect/src/main/java/stri/java_connect/protocol/ProtocoleMessagerie.java
+++ Code/java-connect/src/main/java/stri/java_connect/protocol/ProtocoleMessagerie.java
@@ -3,6 +3,8 @@
  */
 package stri.java_connect.protocol;
 
+import java.util.Date;
+
 /**
  * @author emeric
  *
@@ -12,36 +14,62 @@ public abstract class ProtocoleMessagerie extends ProtocoleGenerique
 	private final static String utilisateursURI = "/utilisateurs";
 	private final static String messagerieURI = "/messagerie";
 	
+	/**
+	 * @param idutilisateur
+	 * @param msg
+	 * @return
+	 */
 	public static String requeteEnvoiMessageDiffere(String idutilisateur, String msg)
 	{
 		return null;
 	}
 	
+	/**
+	 * @return
+	 */
 	public static String requeteConsulterListeUtilisateurConnectes()
 	{
 		return "CONSULTER "+ utilisateursURI;
 	}
 	
+	/**
+	 * @param courriel
+	 * @return
+	 */
 	public static String requeteDetailsUtilisateurConnecte(String courriel)
 	{
 		return "CONSULTER "+ utilisateursURI + "/" + courriel;
 	}
 	
+	/**
+	 * @return
+	 */
 	public static String requeteConsulterListeMessagesManques()
 	{
 		return "CONSULTER "+ messagerieURI;
 	}
 	
+	/**
+	 * @param idmsg
+	 * @return
+	 */
 	public static String requeteConsulterDetailsMessagesManque(String idmsg)
 	{
 		return "CONSULTER "+ messagerieURI + "/" + idmsg;
 	}
 	
+	/**
+	 * @return
+	 */
 	public static String requeteSupprimerListeMessagesManques()
 	{
 		return "SUPPRIMER "+ messagerieURI;
 	}
 	
+	/**
+	 * @param idmsg
+	 * @return
+	 */
 	public static String requeteSupprimerMessageManque(String idmsg)
 	{
 		return "SUPPRIMER "+ messagerieURI + "/" + idmsg;
@@ -49,10 +77,56 @@ public abstract class ProtocoleMessagerie extends ProtocoleGenerique
 	
 	//
 	
+	/**
+	 * @param msg
+	 * @return
+	 */
 	public static String requeteMessageDirect(String msg)
 	{
-		// TODO message direct
-		return null;
+		return "MESSAGE " + new Date().getTime() + " " + msg;
+	}
+	
+	/**
+	 * @param requete
+	 * @return
+	 */
+	public static boolean isMessageDirect(String requete)
+	{
+		return requete.startsWith("MESSAGE ") && extraireDateMessageDirect(requete) != null;
+	}
+	
+	/**
+	 * @param requete
+	 * @return
+	 */
+	public static String extraireTimestampMessageDirect(String requete)
+	{
+		return requete.substring(8).split(" ",1)[0];
+	}
+	
+	/**
+	 * @param requete
+	 * @return
+	 */
+	public static String extraireMessageMessageDirect(String requete)
+	{
+		return requete.substring(8).split(" ",2)[1];
+	}
+	
+	/**
+	 * @param requete
+	 * @return
+	 */
+	public static Date extraireDateMessageDirect(String requete)
+	{
+		try
+		{
+			return new Date(Long.parseLong(extraireTimestampMessageDirect(requete)));
+		}
+		catch (NumberFormatException e)
+		{
+			return null;
+		}
 	}
 	
 	//
