ad054839dd0274aaad11f20445b26b6628604d08
emeric254 <emeric254@live.fr>
2016-02-28 01:31:12 +0100

    menu princip

--- Code/java-connect/src/main/java/stri/java_connect/ihm/Menu.java
+++ Code/java-connect/src/main/java/stri/java_connect/ihm/Menu.java
@@ -7,23 +7,36 @@ import org.apache.pivot.collections.*;
 import org.apache.pivot.util.*;
 import org.apache.pivot.wtk.*;
 
+import stri.java_connect.client.ClientAnnuaire;
+import stri.java_connect.modele.Utilisateur;
+
 public class Menu extends Window implements Application, Bindable
 {
-	private Window window;
+    private ClientAnnuaire client;
+    private Utilisateur utilisateur;
 
+	private Window window;
+	@BXML
 	private PushButton btConnexion;
+	@BXML
 	private PushButton btInscription;
+	@BXML
 	private PushButton btListeProfil;
+	@BXML
 	private PushButton btModifProfil;
+	@BXML
 	private PushButton btMessagerieEmail;
+	@BXML
 	private PushButton btMessagerieP2P;
+	@BXML
 	private BoxPane btBox;
-	
+
 	public Menu()
 	{
-		//
+		//client = new ClientAnnuaire();
+		utilisateur = null;
 	}
- 
+
 	public void initialize(final Map<String, Object> namespace, final URL location, final Resources resources)
 	{
 		btConnexion.getButtonPressListeners().add(connexionListener);
@@ -32,23 +45,36 @@ public class Menu extends Window implements Application, Bindable
 		btModifProfil.getButtonPressListeners().add(modifProfilListener);
 		btMessagerieEmail.getButtonPressListeners().add(messagerieMail);
 		btMessagerieP2P.getButtonPressListeners().add(messagerieP2P);
+		
+		// par defaut pas connecter donc pas ces btn
+		btModifProfil.setVisible(false);
+		btMessagerieEmail.setVisible(false);
+		btMessagerieP2P.setVisible(false);
 	}
- 
+
 	public void startup(final Display display, final Map<String, String> properties) throws Exception
 	{
 		BXMLSerializer bxmlSerializer = new BXMLSerializer();
 		window = (Window) bxmlSerializer.readObject(Menu.class, "/menu.bxml");
 		window.open(display);
 	}
- 
+
 	private final ButtonPressListener connexionListener = new ButtonPressListener()
 	{
 		public void buttonPressed(final Button button)
 		{
+			//
+			// traitement connexion
 			Alert.alert(MessageType.INFO, "Connexion", Menu.this);
+			// connecte
+			btConnexion.setVisible(false);
+			btInscription.setVisible(false);
+			btModifProfil.setVisible(true);
+			btMessagerieEmail.setVisible(true);
+			btMessagerieP2P.setVisible(true);
 		}
 	};
- 
+
 	private final ButtonPressListener inscriptionListener = new ButtonPressListener()
 	{
 		public void buttonPressed(final Button button)
@@ -56,7 +82,7 @@ public class Menu extends Window implements Application, Bindable
 			Alert.alert(MessageType.INFO, "Inscription", Menu.this);
 		}
 	};
- 
+
 	private final ButtonPressListener listeProfilListener = new ButtonPressListener()
 	{
 		public void buttonPressed(final Button button)
@@ -64,7 +90,7 @@ public class Menu extends Window implements Application, Bindable
 			Alert.alert(MessageType.INFO, "Liste profils", Menu.this);
 		}
 	};
- 
+
 	private final ButtonPressListener modifProfilListener = new ButtonPressListener()
 	{
 		public void buttonPressed(final Button button)
@@ -72,7 +98,7 @@ public class Menu extends Window implements Application, Bindable
 			Alert.alert(MessageType.INFO, "Modification profil", Menu.this);
 		}
 	};
-	
+
 	private final ButtonPressListener messagerieMail = new ButtonPressListener()
 	{
 		public void buttonPressed(final Button button)
@@ -80,26 +106,26 @@ public class Menu extends Window implements Application, Bindable
 			Alert.alert(MessageType.INFO, "Messagerie Mail", Menu.this);
 		}
 	};
-	
+
 	private final ButtonPressListener messagerieP2P = new ButtonPressListener()
 	{
 		public void buttonPressed(final Button button)
 		{
-			Alert.alert(MessageType.INFO, "Messagerie peer to peerl", Menu.this);
+			Alert.alert(MessageType.INFO, "Messagerie peer to peer", Menu.this);
 		}
 	};
- 
+
 	public boolean shutdown(final boolean optional) throws Exception
 	{
 		this.close();
 		return false;
 	}
- 
+
 	public void suspend() throws Exception
 	{
 		//
 	}
- 
+
 	public void resume() throws Exception
 	{
 		//
