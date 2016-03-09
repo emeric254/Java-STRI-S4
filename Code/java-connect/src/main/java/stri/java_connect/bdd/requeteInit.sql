d4c082072f3b32f0ea66b42edde120f6a28da4d2
remikaze <remi.b7@hotmail.fr>
2016-03-09 20:36:55 +0100

    ajout script création tables

--- Code/java-connect/src/main/java/stri/java_connect/bdd/requeteInit.sql
+++ Code/java-connect/src/main/java/stri/java_connect/bdd/requeteInit.sql
@@ -12,15 +12,29 @@ create table utlisateur (courriel string primary key not null,
                         )
 
 
+create table competence (idCompetence integer auto_increment not_null,
+			nomCompetence string,
+			idCompetence  primary key)
+
+create table posseder (courriel string,
+			idCompetence integer)
+
+create table envoyerMessage (courrielSource string not null,
+				courrielDestinataire string not null,
+				dateMessage timestamp not null,
+				message text,
+				foreing key (corrielSource) references utilisateur(courriel),
+				foreing key (courrielDestinataire) references utilisateur(courriel),
+				primary key (courrielSource,courrielDestinataire,dateMessage)
+				)
 
 
-create table competence (
-
-
-
-
-create table aimer (courrielLikant string primary key not null,
-                    idCompetence string primary key not null,
-                    courrielPropCompetence string primary key not null)
+create table aimer (courrielLikant string not null,
+                    idCompetence string not null,
+                    courrielPropCompetence string not null,
+		foreing key (courrielLikant) references utilisateur(courriel),
+		foreing key (courrielPropCompetence) references utilisateur(courriel),
+		primary key (courrielLikant,courrielPropCompetence,idCompetence)
+		)
 
 
